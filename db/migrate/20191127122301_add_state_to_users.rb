922990eb134350268dea2510e68abd2d435ce8fc
harshitranawat <harshit@exalogic.in>
2019-11-28 18:20:26 +0530

    Initial Commit

--- db/migrate/20191127122301_add_state_to_users.rb
+++ db/migrate/20191127122301_add_state_to_users.rb
@@ -1,7 +1,7 @@
 class AddStateToUsers < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :state, :string
-    add_column :users, :pincode, :number
+    add_column :users, :pincode, :integer
     add_column :users, :country, :string
     add_column :users, :gender, :string
   end
